fc15d89ecf6bb924f9525da39762717e11f7f937
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-19 09:10:32 +0100

    Static values

--- Views/WeatherView.swift
+++ Views/WeatherView.swift
@@ -27,7 +27,7 @@ struct WeatherView: View {
                 
                 VStack {
                     Text("19")
-                        .font(.system(size: 66, weight: .semibold))
+                        .font(.system(size: 86, weight: .semibold))
                     
                     Text("Rainy")
                         .font(.system(size: 26))
@@ -37,6 +37,14 @@ struct WeatherView: View {
                 Spacer()
             }
             .padding()
+            
+            // Add Cards to show humidity, wind etc.
+            Spacer()
+            CardView(icon: "thermometer", text: "Min temp", value: 12.0)
+            CardView(icon: "thermometer", text: "Max temp", value: 12.0)
+            CardView(icon: "wind", text: "Wind speed", value: 12.0)
+            CardView(icon: "humidity", text: "Humidity", value: 12.0)
+            
         }
         .padding()
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
@@ -52,5 +60,34 @@ struct WeatherView: View {
 extension Double {
     func roundDouble() -> String {
         return String(format: "%.0f", self)
+    }
+}
+
+struct CardView : View {
+    var icon: String
+    var text: String
+    var value: Double
+    
+    var body: some View {
+        HStack(spacing: 20 ) {
+            Image(systemName: icon)
+                .imageScale(.large)
+                .frame(width: 50, height: 50)
+                .background(.white.opacity(0.7))
+                .cornerRadius(12)
+            
+            Text(text)
+                .font(.system(size: 15))
+            
+            Spacer()
+            
+            Text("\(value.roundDouble())")
+                .font(.headline)
+        }
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity)
+        .frame(height: 70)
+        .background(.white.opacity(0.3))
+        .cornerRadius(12)
     }
 }
