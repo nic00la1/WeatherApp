ac914f6226c0f3352ad2739893ac3548beec2de7
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-19 08:33:14 +0100

    Response Body weatherManager

--- Managers/WeatherManager.swift
+++ Managers/WeatherManager.swift
@@ -24,3 +24,43 @@ class WeatherManager {
     }
 }
 
+struct ResponseBody : Decodable {
+    
+    var coord: CoordinateResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+    
+    struct CoordinateResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+    
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+    
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
+}
