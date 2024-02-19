4d27b74511f4b341de98a25adae7f8c40e03ec69
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-19 08:49:37 +0100

    modelData

---
+++ Managers/ModelData.swift
@@ -0,0 +1,29 @@
+//
+//  ModelData.swift
+//  WeatherApp
+//
+//  Created by Nicola Kaleta on 19/02/2024.
+//
+
+import SwiftUI
+var previewWeather: ResponseBody = load("weatherData.json")
+
+func load<T:Decodable>(_ filename: String) -> T {
+    let data: Data
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
